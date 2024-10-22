55561c422cb71b3adca342c52098d0888a0ba6e2
shahmaran0207 <shahmaran0207@naver.com>
2024-10-22 14:59:16 +0900

    edit memo complete

---
+++ backend/api-server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser=require("body-parser");
+
+const memos=[];
+
+app.use(bodyParser.json());
+
+app.get('/api/memos', (req, res) => {
+    res.send(memos)
+})
+
+app.post("/api/memos", (req, res)=>{
+    memos.push(req.body.content);
+    res.send(memos);
+});
+
+app.put("/api/memos:idx", (req, res)=>{
+    memos[req.params.idx] = req.body.content;
+    res.send(memos);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
--- frontend/src/components/AppMemo.vue
+++ frontend/src/components/AppMemo.vue
@@ -4,13 +4,14 @@
       <button class="btn btn-primary" @click="add">추가</button>
     </div>
     <ul>
-      <li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
+      <li v-for="(d, idx) in state.data" :key="idx" @click="edit(idx)">{{ d }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 import { reactive } from 'vue';
+import axios from "axios";
 
 export default {
   setup() {
@@ -20,10 +21,26 @@ export default {
     });
 
     const add = () => {
-      state.data.push("추가된 메모 내용");
+      const content=prompt("내용을 입력해 주세요");
+
+      axios.post("/api/memos", {content: content}).then((res)=>{
+        state.data=res.data;
+      });
     };
 
-    return { state, add };
+    //put: 일부 내용을 수정할 때 사용
+    const edit=(idx)=>{
+      const content=prompt("내용을 입력해 주세요", state.data[idx]);
+      axios.put("/api/memos"+idx,{content}).then((res)=>{
+        state.data=res.data;
+      });
+    };
+
+    axios.get("/api/memos").then((res)=>{
+      state.data=res.data;
+    });
+
+    return { state, add, edit };
   },
 };
 </script>
@@ -37,12 +54,12 @@ export default {
 
   ul {
     list-style: none;
-    padding: 15px 0;
+    padding: 15px;
     margin: 0;
 
     li {
       padding: 15px;
-      margin: 5px;
+      margin: 10px 0;
       border: 1px solid #eeeeee;
     }
   }
