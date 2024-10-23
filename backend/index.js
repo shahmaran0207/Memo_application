c8b5b68d0555336b4de4eb341d90567ef7406439
shahmaran0207 <shahmaran0207@naver.com>
2024-10-23 13:16:22 +0900

    deploy start

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const database = require("./database");
 
 app.use(bodyParser.json());
 
+app.use(express.static('dist'))
+
 app.get('/api/memos', async (req, res) => {
     try {
         const result = await database.run("SELECT * FROM memos");
@@ -14,7 +16,6 @@ app.get('/api/memos', async (req, res) => {
         res.status(500).send("Database error");
     }
 });
-
 
 app.post("/api/memos", async (req, res) => {
     await database.run(`INSERT INTO memos (content) VALUES (?)`, [req.body.content]);
